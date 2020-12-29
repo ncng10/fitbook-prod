24f2caa794613812715ae4582fafba7c63bcd12e
Nolan Cong <ncongnolancong@gmail.com>
2020-12-28 20:50:07 -0800

    if users goes to the public user profile router with their username, they will be pushed to their profile

--- fitbook-frontend/src/pages/users/[username].tsx
+++ fitbook-frontend/src/pages/users/[username].tsx
@@ -1,8 +1,9 @@
 import { Box } from '@chakra-ui/react';
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react'
 import BottomNavigation from '../../components/MobileViews/BottomNavigation';
 import PublicUserProfileCard from '../../components/MobileViews/PublicUserProfileCard';
-import { usePublicUserProfileQuery } from '../../generated/graphql';
+import { useMeQuery, usePublicUserProfileQuery } from '../../generated/graphql';
 import { useGetUsernameUrl } from '../../utils/useGetUsernameUrl';
 import { withApollo } from '../../utils/withApollo';
 
@@ -11,15 +12,19 @@ interface PublicUserProfileProps {
 }
 
 const PublicUserProfile: React.FC<PublicUserProfileProps> = ({ }) => {
-
+    const router = useRouter();
     const userName = useGetUsernameUrl();
-
+    const { data: meData } = useMeQuery();
     const { data } = usePublicUserProfileQuery({
         variables: {
             input: userName
         }
     });
-    console.log("username", userName)
+    useEffect(() => {
+        if (userName === meData?.me.username) {
+            router.push("/profile")
+        }
+    })
     return (
         <React.Fragment>
             <Box>
